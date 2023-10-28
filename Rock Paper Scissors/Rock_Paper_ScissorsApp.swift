2d387e3c68b7f4a3eda2880afac84cf2c813b1f0
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-10-28 14:31:43 -0400

    starting with app template

---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Bakhtiyorjon Mirzajonov on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
